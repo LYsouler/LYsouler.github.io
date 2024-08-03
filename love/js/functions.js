e3d96ad654ceed6bfe5e287b8ea8044f11ed0ed8
LYsouler <LLL12861@163.com>
2024-08-03 17:51:44 +0800

    Update functions.js

--- love/js/functions.js
+++ love/js/functions.js
@@ -110,6 +110,8 @@ function timeElapse(date){
     if (seconds < 10) {
         seconds = "0" + seconds;
     }
+    console.log(hours);
+    console.log(seconds);
     var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds";
     $("#elapseClock").html(result);
 }
